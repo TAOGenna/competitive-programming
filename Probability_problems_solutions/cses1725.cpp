8f628aa5092d05ccc97f181886d39c37d0f4206f
TAOGenna <a20161748@pucp.edu.pe>
2024-11-25 11:00:28 -0500

    change

---
+++ Probability_problems_solutions/cses1725.cpp
@@ -0,0 +1,131 @@
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+
+using namespace std;
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x);
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? ", " : ""), __print(i);
+    cerr << "}";
+}
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ", ";
+    __print(x.second);
+    cerr << '}';
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifdef DEBUG
+#define trace(x...)                                                           \
+    cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = ["; \
+    _print(x);                                                                \
+    cerr << "\e[39m" << endl;
+#else
+#define trace(x...)
+#endif
+
+// #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cin.exceptions(cin.failbit);
+#define rep(i, a, b) for (ll i = a; i < (b); ++i)
+#define trav(a, x) for (auto &a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define ff first
+#define ss second
+#define pb push_back
+#define ins insert
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define wis cout << endl \
+                 << "I already speak english bitch" << end << endl;
+#define line cout << "------------------------------" << endl;
+
+typedef long long int ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+template <class T>
+bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T>
+bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// use: g++ -DDEBUG -o code code.cpp
+
+const int ROLLS = 105;
+const int MAX   = 610;
+double dp[ROLLS][MAX];
+
+int main(){
+    int n,a,b; cin>>n>>a>>b;
+    double cte = 1.0/6.0;
+    dp[0][0] = 1.0;
+    rep(i,1,n+1){
+        rep(j,1,MAX){
+            for(int k=1;k<=6;k++){
+                if(j-k<0) continue; 
+                dp[i][j]+=dp[i-1][j-k]*cte;
+            }
+        }
+    }
+
+    double ans = 0;
+    for(int i=a;i<=b;i++) ans += dp[n][i];
+
+    cout<<setprecision(6);
+    cout<<fixed;
+    cout<<ans<<endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Useful links
-[Keep track of your solutions in AtCoder](https://kenkoooo.com/atcoder/#/table/)<br>
-[Collection of OI problems from several competitions](https://oichecklist.pythonanywhere.com/)<br>
-[my sensei](https://github.com/eduardo-vp/Competitive-Programming?fbclid=IwAR0vldsze5AQuLH0Iq3Siy7X-f-WABRz6zsydAhnUQB18OJXmtc9K9AszIg)
+- [Keep track of your solutions in AtCoder](https://kenkoooo.com/atcoder/#/table/)
+- [Collection of OI problems from several competitions](https://oichecklist.pythonanywhere.com/)
+- [my sensei](https://github.com/eduardo-vp/Competitive-Programming?fbclid=IwAR0vldsze5AQuLH0Iq3Siy7X-f-WABRz6zsydAhnUQB18OJXmtc9K9AszIg)
